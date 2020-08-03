4d62c97ce530d3e7a80fe00c20e98f7d90855235
AlfaGeomatics <60299834+AlfaGeomatics@users.noreply.github.com>
2020-08-03 07:39:54 -0600

    Add files via upload

---
+++ data/Fbricadebateras_3.js
@@ -0,0 +1,8 @@
+var json_Fbricadebateras_3 = {
+"type": "FeatureCollection",
+"name": "Fbricadebateras_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "Tipo": "Ex Fábrica de baterías RECORD" }, "geometry": { "type": "Point", "coordinates": [ -89.370517341074802, 13.792181122743438 ] } }
+]
+}
